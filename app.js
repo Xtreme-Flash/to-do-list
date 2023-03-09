b75415350de518fe5896df00343de42b4d5bd302
Xtreme-Flash <doorway125@gmail.com>
2023-03-09 12:37:30 +0530

    added mongoose module

--- app.js
+++ app.js
@@ -2,14 +2,35 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const { getDate } = require("./date");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+
 
 
 const app = express();
 
-let items = [];
-let workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+    name: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const list1 = new Item ({
+    name: "holy play"
+});
+
+const list2 = new Item({
+    name: "maked legend video."
+});
+
+const list3 = new Item({
+    name: "study."
+});
+
+const defaulItem = [list1, list2, list3];
+
+Item.insertMany(defaulItem);
 
 app.set("view engine", "ejs");
 
@@ -18,9 +39,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-    let day = getDate();
-    
-    res.render("list", {listTitle: day, newListItems: items});
+    res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
